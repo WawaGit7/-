8627ea07a2cbd6ba0581a40a62574d04ce236a69
WawaGit7 <81234958+WawaGit7@users.noreply.github.com>
2023-01-31 20:53:10 +0800

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 func main() {
-	// fmt.Println("HELLO WORLD")
-	rand.Seed(time.Now().UnixNano())
-	m := rand.Intn(100000)
-	fmt.Println(m)
+	fmt.Println("HELLO WORLD")
 }
